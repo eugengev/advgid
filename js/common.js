0c3b96d4bc537c69ab56e6b7ec9d25cfd0147cdf
eugengev <eugengev@gmail.com>
2019-04-11 17:53:17 +0300

    commit

--- js/common.js
+++ js/common.js
@@ -47,6 +47,41 @@ $(function() {
 //-----------------------------------------------------------------------------
     $('input[placeholder], textarea[placeholder]').placeholder();
 
+    $('.js-cliet-slider').slick({
+      dots: true,
+      infinite: false,
+      speed: 300,
+      slidesToShow: 7,
+      slidesToScroll: 7,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
 
 });
 
