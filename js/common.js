7a207ce386bbdc0c220c7c5b9f38e949c4472bcf
eugengev <eugengev@gmail.com>
2019-04-17 01:26:15 +0300

    commit

--- js/common.js
+++ js/common.js
@@ -94,6 +94,16 @@ $(function() {
       });
     }
 
+    $('.rewievs_about__slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        nextArrow: '<button type="button" class="slick-next">&rarr;</button>',
+        prevArrow: '<button type="button" class="slick-prev">&larr;</button>',
+        adaptiveHeight: true
+    });
+
 });
 
 var map;
